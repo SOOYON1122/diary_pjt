053feffd06579878bcdf5173aedc8dfa8789af71
Jung Jieun <wldms3333@gmail.com>
2024-10-21 11:14:41 +0900

    24.10.21 11:14 지은 - 리다이렉트 수정, 친구 삭제, 내가 한 요청 보기 기능 추가

--- friends/urls.py
+++ friends/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
   path('my-friends/', views.myfriends, name="my_friends"),
   path('reject-friend/<int:friend_id>/', views.rejectfriend, name="reject_friend"),
   path('accept-friend/<int:friend_id>/', views.acceptfriend, name="accept_friend"),
+  path('friend-request-cancel/<int:friend_id>/', views.cancelfriend, name='friend_request_cancel'),
+  path('delete-friend/<int:friend_id>/', views.deletefriend, name='delete_friend'),
 ]
